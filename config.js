c41abb16d0082b91ac6e2df993d604eba0401c45
halimbaykus <95928916+halimbaykus@users.noreply.github.com>
2021-12-10 18:06:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 module.exports = {
         px: '.',
-        token: 'OTE4ODkyNTg2NTU3NjUzMDIy.YbN3aA.pj2WX2hN9lDwI_sz1UUatc4KNds', //DİSCORD BOTUNUZUN TOKENİNİ YAZIN
+        token: 'OTE4ODkyNTg2NTU3NjUzMDIy.YbN3aA.pj2WX2hN9lDwI_sz1UUatc4KNds', 
         playing: 'MUPSIC',
 
     opt: {
         DJ: {
-            enabled: false, //EĞER SADECE DJLER KULLANA BİLSİN İSTİYOR İSENİZ false yazanı true yapın.
-            roleName: 'DJ', //DJ ROLÜNÜN İSMİ NE OLACAK İSE YAZIN SUNUCUNUZDA O ROLDEKİLER KULLANA BİLİR
-            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume'] //DOKUNMA
+            enabled: false, 
+            roleName: 'DJ', 
+            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume']
         },
-        maxVol: 250, //maximum ses seviyesi kaç olacak belirte bilirsiniz.
-        loopMessage: false, //elleme
+        maxVol: 250, 
+        loopMessage: false,
         discordPlayer: {
             ytdlOptions: {
-                quality: 'highestaudio', //elleme
-                highWaterMark: 1 << 25 //elleme
+                quality: 'highestaudio', 
+                highWaterMark: 1 << 25 
             }
         }
     }
